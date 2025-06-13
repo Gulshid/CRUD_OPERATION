24908b8ed16666b8d94c6f65e192cc03e123e950
Gulshid Zada <gulshidzada4@gmail.com>
2025-06-13 21:19:31 +0500

    Crud Operation Started

--- crud_operation/lib/View_Model/crud_provider.dart
+++ crud_operation/lib/View_Model/crud_provider.dart
@@ -1,29 +1,194 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:another_flushbar/flushbar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CrudProvider with ChangeNotifier{
-  final DatabaseReference _dbRef = FirebaseDatabase.instance.ref('items');
+  final firstNameController = TextEditingController();
+  final lastNameController = TextEditingController();
 
-  // CREATE
-  Future<void> createItem(String id, Map<String, dynamic> data) async {
-    await _dbRef.child(id).set(data);
+  final updatefirstName = TextEditingController();
+  final updatelastName = TextEditingController();
+
+  bool isloading = false;
+
+  //create data fuunction
+  Future<void> createData(BuildContext context) async {
+    isloading = true;
     notifyListeners();
+    try {
+      await FirebaseFirestore.instance.collection('CRUD OPERATION').add({
+        'firstName':firstNameController.text,
+        'lastName':lastNameController.text,
+      });
+      print('Data created successfully');
+
+      Flushbar(
+        message: "Data Created Successfully ",
+        icon: Icon(Icons.check_circle, color: Colors.white),
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.blue,
+        margin: EdgeInsets.all(8),
+        borderRadius: BorderRadius.circular(8),
+        flushbarPosition: FlushbarPosition.TOP, // or BOTTOM
+      ).show(context);
+      isloading = false;
+      notifyListeners();
+    } catch (e) {
+      print('Error creating data: $e');
+      Flushbar(
+        message: "Data Created failed to create",
+        icon: Icon(Icons.check_circle, color: Colors.white),
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.red,
+        margin: EdgeInsets.all(8),
+        borderRadius: BorderRadius.circular(8),
+        flushbarPosition: FlushbarPosition.TOP, // or BOTTOM
+      ).show(context);
+      isloading = false;
+      notifyListeners();
+    } finally {
+      isloading = false;
+      notifyListeners();
+    }
   }
 
-  // READ (Stream for real-time updates)
-  Stream<DatabaseEvent> readItems() {
-    return _dbRef.onValue;
+  //read data function
+  Stream<QuerySnapshot> readData(BuildContext context) {
+    return FirebaseFirestore.instance.collection('CRUD OPERATION').snapshots();
   }
 
-  // UPDATE
-  Future<void> updateItem(String id, Map<String, dynamic> data) async {
-    await _dbRef.child(id).update(data);
-    notifyListeners();
+  //update data dialoge box
+  Future<void> updateDialoge(BuildContext context, String id) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Update Data', style: TextStyle(color: Colors.blue)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  controller: updatefirstName,
+                  style: TextStyle(color: Colors.black),
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Update first Name',
+                    hintText: 'Enter update first Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(13.r),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.h),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  controller: updatelastName,
+                  style: TextStyle(color: Colors.black),
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Update last Name',
+                    hintText: 'Enter update last Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(13.r),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('No'),
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                updateData(id, context);
+                updatefirstName.clear();
+                updatelastName.clear();
+                Navigator.pop(context);
+              },
+              child: Text('Update'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
-  // DELETE
-  Future<void> deleteItem(String id) async {
-    await _dbRef.child(id).remove();
-    notifyListeners();
+  // update data function
+
+  Future<void> updateData(String id, BuildContext context) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('CRUD OPERATION')
+          .doc(id)
+          .update({
+            'firstName': updatefirstName.text,
+            'lastName': updatelastName.text,
+          });
+      print('Data Updated Successfully');
+      Flushbar(
+        message: "Data Updated Successfully ",
+        icon: Icon(Icons.check_circle, color: Colors.white),
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.blue,
+        margin: EdgeInsets.all(8),
+        borderRadius: BorderRadius.circular(8),
+        flushbarPosition: FlushbarPosition.TOP, // or BOTTOM
+      ).show(context);
+    } catch (e) {
+      // ignore: avoid_print
+      print('Error updating data: $e');
+      Flushbar(
+        message: "Data Updated issue  ",
+        icon: Icon(Icons.check_circle, color: Colors.white),
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.red,
+        margin: EdgeInsets.all(8),
+        borderRadius: BorderRadius.circular(8),
+        flushbarPosition: FlushbarPosition.TOP, // or BOTTOM
+      ).show(context);
+    }
+  }
+
+  //delete data function
+  Future<void> deleteData(String id, BuildContext context) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('CRUD OPERATION')
+          .doc(id)
+          .delete();
+      print('Data deleted successfully');
+      Flushbar(
+        message: "Data Deleted Successfully ",
+        icon: Icon(Icons.check_circle, color: Colors.white),
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.blue,
+        margin: EdgeInsets.all(8),
+        borderRadius: BorderRadius.circular(8),
+        flushbarPosition: FlushbarPosition.TOP, // or BOTTOM
+      ).show(context);
+    } catch (e) {
+      print('Error deleting data: $e');
+      Flushbar(
+        message: "Data Deleted issue  ",
+        icon: Icon(Icons.check_circle, color: Colors.white),
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.red,
+        margin: EdgeInsets.all(8),
+        borderRadius: BorderRadius.circular(8),
+        flushbarPosition: FlushbarPosition.TOP, // or BOTTOM
+      ).show(context);
+    }
   }
 }
--- crud_operation/lib/view_/home_view.dart
+++ crud_operation/lib/view_/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:crud_operation/View_Model/crud_provider.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class HomeView extends StatefulWidget {
@@ -15,58 +16,231 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
       final crud_provider = Provider.of<CrudProvider>(context);
     return Scaffold(
-      body: StreamBuilder<DatabaseEvent>(
-        stream: crud_provider.readItems(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data!.snapshot.value != null) {
-            final items = Map<String, dynamic>.from(
-              snapshot.data!.snapshot.value as Map,
-            );
-            return ListView(
-              children: items.entries.map((entry) {
-                return ListTile(
-                  title: Text(entry.value['name']),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {
-                          crud_provider.updateItem(entry.key,{'name': 'updated name'} );
-                          // Call updateItem here
-                        },
+      backgroundColor: Colors.deepPurpleAccent,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(
+              'Crud Operation',
+              style: GoogleFonts.aBeeZee(
+                fontSize: 30.sp,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            centerTitle: true,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(17.r),
+                topRight: Radius.circular(17.r),
+                bottomLeft: Radius.circular(17.r),
+                bottomRight: Radius.circular(17.r),
+              ),
+            ),
+            backgroundColor: Colors.white,
+            floating: true,
+            pinned: true,
+
+            expandedHeight: 150.h,
+            flexibleSpace: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(17.r),
+                    topRight: Radius.circular(17.r),
+                    bottomLeft: Radius.circular(17.r),
+                    bottomRight: Radius.circular(17.r),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.indigo, Colors.purple, Colors.cyan],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
                       ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          crud_provider.deleteItem(entry.key);
-                               // Call updateItem here
-                        },
+                    ),
+                    child: FlexibleSpaceBar(
+                      background: Column(
+                        children: [
+                          SizedBox(height: 100.h),
+                          Text(
+                            'Welcome to New Users',
+                            style: GoogleFonts.abel(
+                              color: Colors.orange,
+                              fontSize: 30.sp,
+                            ),
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          crud_provider.deleteItem(entry.key);
-                        },
-                      ),
-                    ],
+                    ),
                   ),
                 );
-              }).toList(),
-            );
-          }
-          return Center(child: Text('No data'));
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          final newId = FirebaseDatabase.instance.ref('items').push().key;
-          if (newId != null) {
-            crud_provider.createItem(newId, {'name': 'New Item'});
-          }
-        },
-        child: Icon(Icons.add),
-      ),
+              },
+            ),
+          ),
+      
+        SliverToBoxAdapter(
+          child:Container(
+            height: 520.h,
+                decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.indigo, Colors.purple, Colors.cyan],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(17.r),
+                  topRight: Radius.circular(17.r),
+                ),
+              ),
+            child: Column(
+              children: [
+                SizedBox(height: 50.h,),
+                Text(
+              'CRUD Operation',
+              style: TextStyle(
+                fontSize: 30.sp,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 20.h),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: crud_provider.firstNameController,
+                style: TextStyle(color: Colors.black),
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'FirstName',
+                  labelStyle: TextStyle(color: Colors.black),
+                  hintStyle: TextStyle(color: Colors.white),
+                  hintText: 'Enter  first name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13.r),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 30.sp),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: crud_provider.lastNameController,
+                style: TextStyle(color: Colors.black),
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'LastName',
+                  hintText: 'Enter last Name',
+                  labelStyle: TextStyle(color: Colors.black),
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13.r),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 30.h),
+            GestureDetector(
+              onTap: () async {
+                await crud_provider.createData(context);
+              },
+              child: Container(
+                width: double.infinity,
+                height: 50.h,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+
+                child:
+                    crud_provider.isloading
+                        ? Center(child: CircularProgressIndicator())
+                        : Center(
+                          child: Text(
+                            'Add',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+              ),
+            ),
+
+            Divider(),
+            Expanded(
+              child: StreamBuilder(
+                stream: crud_provider.readData(context),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(
+                    child: Text(
+                    'data error',
+                    style: TextStyle(color: Colors.red),
+                    ),
+                    );
+                  } else if (snapshot.hasData) {
+                    final datas = snapshot.data!.docs;
+                    if (datas.isEmpty) {
+                    return Center(
+                    child: Text(
+                    'no data found',
+                    style: TextStyle(color: Colors.red),
+                    ),
+                    );
+                    }
+                    return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                    final data = snapshot.data!.docs[index];
+                    return Card(
+                    child: ListTile(
+                    title: Text(
+                    data['firstName'].toString(),
+                    style: TextStyle(color: Colors.black),
+                    ),
+                    subtitle: Text(
+                    data['lastName'].toString(),
+                    style: TextStyle(color: Colors.black),
+                    ),
+                    trailing: Row(
+                    children: [
+                    GestureDetector(
+                    onTap: () {
+                    crud_provider.updateDialoge(
+                    context,
+                    data.id,
+                    );
+                    },
+                    child: Icon(Icons.edit, color: Colors.blue),
+                    ),
+                    SizedBox(width: 10.w),
+                    GestureDetector(
+                    onTap: () {
+                    crud_provider.deleteData(data.id, context);
+                    },
+                    child: Icon(Icons.delete, color: Colors.red),
+                    ),
+                    ],
+                    ),
+                    ),
+                    );
+                    }
+                    );
+
+                  }
+ return Container(); // Add a default return statement
+                },
+              ),
+            ),
+                
+              ],
+            ),
+          ) ,)      
+        ],
+      )
       
     );
   }
